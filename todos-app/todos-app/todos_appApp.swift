c67920901f1df8a651c4c0bd17b80e4e53fe3e7c
santoryu24 <amleakhilesh@gmail.com>
2024-04-15 23:54:19 +0530

    initial setup

---
+++ todos-app/todos-app/todos_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todos_appApp.swift
+//  todos-app
+//
+//  Created by Akhilesh Amle on 15/04/24.
+//
+
+import SwiftUI
+
+@main
+struct todos_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
